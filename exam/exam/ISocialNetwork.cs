183033e5e453b0aaa78e2ea961831fab507aca3c
Василий Щербаков <v.sherbakov@softpro.com>
2020-09-30 09:00:33 +0300

    add another type task

---
+++ exam/exam/ISocialNetwork.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace exam
+{
+    public interface ISocialNetwork
+    {
+        void addPerson(string name);
+        void addConnection(string from, string to);
+        List<string> getFriends(string from, int level);
+    }
+
+    public class SocialNetwork : ISocialNetwork
+    {
+        private Dictionary<string, List<string>> _memberWithFriends;
+
+        public SocialNetwork()
+        {
+            _memberWithFriends = new Dictionary<string, List<string>>();
+        }
+        
+        public void addPerson(string name)
+        {
+            if(name is null || name == string.Empty)
+                throw new ArgumentException("Name is empty or null...");
+            
+            if(_memberWithFriends.ContainsKey(name))
+                throw new ArgumentException("Member with this key already added");
+            
+            _memberWithFriends.Add(name, new List<string>());
+        }
+
+        public void addConnection(string from, string to)
+        {
+            if(from is null || from == string.Empty)
+                throw new ArgumentException("From is empty or null...");
+            
+            if(to is null || to == string.Empty)
+                throw new ArgumentException("To is empty or null...");
+
+            if(!_memberWithFriends.ContainsKey(from))
+                throw new ArgumentException("This member didnt found...");
+            
+            _memberWithFriends[from].Add(to);
+        }
+
+        public List<string> getFriends(string from, int level)
+        {
+            if(from is null || from == string.Empty)
+                throw new ArgumentException("From is empty or null...");
+            
+            if(!_memberWithFriends.ContainsKey(from))
+                throw new ArgumentException("This member didnt found...");
+            
+            if(level < 1)
+                throw new ArgumentException("Level must be positive not null number");
+
+            if (level == 1)
+                return _memberWithFriends[from];
+
+            var friends = new List<string>();
+            
+            foreach (var friend in _memberWithFriends[from])
+            {
+                friends.Add(friend);
+                friends.AddRange(getFriends(friend, level -1));
+            }
+
+            return friends;
+        }
+    }
+}
---
+++ exam/exam/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace exam
+{
+    struct Product
+    {
+        public string Name;
+        public string Country;
+        public double Price;
+        public DateTime Date;
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fullFileName = @"C:\input.txt";
+            
+            if (!File.Exists(fullFileName))
+            {
+                Console.WriteLine($"File {fullFileName} not found");
+                return;
+            }
+
+            var products = new List<Product>();
+            
+            foreach (var line in File.ReadLines(fullFileName))
+            {
+                var splitedLine = line.Split(" ");
+
+                var product = new Product
+                {
+                    Name = splitedLine[0],
+                    Country = splitedLine[1],
+                    Price = double.Parse(splitedLine[2]),
+                    Date = DateTime.Parse(splitedLine[3])
+                };
+                
+                products.Add(product);
+            }
+
+            var productsByPrices = products.OrderBy(product => -product.Price);
+            
+             foreach (var product in productsByPrices)
+             {
+                 Console.WriteLine($"{product.Name} {product.Country} {product.Price} {product.Date}");
+             }
+
+             Console.WriteLine(DateTime.Parse("04.08.2020").Subtract(TimeSpan.FromDays(50)));
+             
+             var freshProducts = products.Where(product => product.Date.CompareTo(DateTime.Now.Subtract(TimeSpan.FromDays(50))) < 0);
+
+             foreach (var product in freshProducts)
+             {
+                 Console.WriteLine($"{product.Name} {product.Country} {product.Price} {product.Date}");
+             }
+        }
+    }
+}
