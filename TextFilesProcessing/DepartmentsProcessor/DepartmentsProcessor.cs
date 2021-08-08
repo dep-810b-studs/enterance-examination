38429d7c08515360d2ed5bc36531fc84c8210d7d
sh-vasily <vasia.punk.rok@gmail.com>
2021-08-08 12:44:00 +0300

    add departments processor

---
+++ TextFilesProcessing/DepartmentsProcessor/DepartmentsProcessor.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace TextFilesProcessing
+{
+    public record Department(string Title, string HeadLastName, int GraduatedBachelorsVolume, uint Year)
+    {
+        public static Department GetFromLine(string line)
+        {
+            var splittedLine = line.Split(" ");
+            
+            var title = splittedLine[0];
+            var headLastName = splittedLine[1];
+            var graduatedBachelorsVolume = int.Parse(splittedLine[2]);
+            var year = uint.Parse(splittedLine[3]);
+
+            return new(title, headLastName, graduatedBachelorsVolume, year);
+        }
+    }
+
+    public static class DepartmentsProcessor
+    {
+        public static void ProcessFile(string fullFileName)
+        {
+            if (!File.Exists(fullFileName))
+            {
+                throw new FileNotFoundException("File with departments didn't found", fullFileName);
+            }
+
+            var departments = File.ReadLines(fullFileName)
+                .Select(Department.GetFromLine)
+                .ToList();
+
+            var averageGraduatedBachelorsVolume = departments
+                .Select(department => department.GraduatedBachelorsVolume)
+                .Average();
+
+            var needPercentage = (averageGraduatedBachelorsVolume / 100) * 15;
+
+            var needDepartments = departments
+                .GroupBy(department => department.Title)
+                .Select(department =>
+                {
+                    var averageDepartmentGraduatedBachelorsVolume = department
+                        .Select(currentDepartment => currentDepartment.GraduatedBachelorsVolume)
+                        .Average();
+
+                    return (department.Key, averageDepartmentGraduatedBachelorsVolume);
+                })
+                .Where(department =>
+                    (averageGraduatedBachelorsVolume -  department.averageDepartmentGraduatedBachelorsVolume) == needPercentage)
+                .OrderBy(department => department.Key)
+                .Select(department => department.Key)
+                .ToList();
+
+            foreach (var department in needDepartments)
+            {
+                Console.WriteLine($"Department {department}");
+            }
+        }
+    }
+}
